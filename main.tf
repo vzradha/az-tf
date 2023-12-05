bd708a0b8588a9137ad7fe0fcc10646cbdcb6bf4
Valli <vzradha@vallis-mbp.lan>
2023-12-06 07:43:52 +1100

    add vnet

--- main.tf
+++ main.tf
@@ -6,3 +6,22 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = random_pet.rg_name.id
 }
+
+resource "azurerm_virtual_network" "taurus" {
+  name = "taurus-network"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space = ["172.20.0.0/16"]
+  dns_servers = ["172.20.0.4", "172.20.0.5"]
+
+  subnet {
+    name = "taurus-subnet1"
+    address_prefix = "172.20.1.0/24"
+  }
+
+  subnet {
+    name = "taurus-subnet2"
+    address_prefix = "172.20.2.0/24"
+  }
+
+}
--- providers.tf
+++ providers.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">=0.12"
+cloud {
+    organization = "mesostech"
 
+    workspaces {
+      name = "az-tf"
+    }
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
